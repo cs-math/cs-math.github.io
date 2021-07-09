13d3128232bdd4ae7918c863da999675bb860dc4
Brikaa <brikaaomar@gmail.com>
2021-07-09 17:35:26 +0200

    Latest changes

--- quadratic/calc.js
+++ quadratic/calc.js
@@ -1,6 +1,8 @@
-function calculate()
-{
+function calculate() {
+    const DECIMAL_PLACES = 4;
     let a, b, c;
+    document.getElementById("root1").innerHTML = "x<sub>1</sub> = ";
+    document.getElementById("root2").innerHTML = "x<sub>2</sub> = ";
     if(document.getElementById("box-b").value.length === 0) {
         b = 0;
     }
@@ -13,8 +15,8 @@ function calculate()
     c = parseInt(document.getElementById("box-c").value);
 
     if (document.getElementById("box-a").value.length === 0 || a === 0) {
-        document.getElementById("root1").innerHTML = "X1 = " + String(-c / b);
-        document.getElementById("root2").innerHTML = "X2 = N/A";
+        document.getElementById("root1").innerHTML += String(-c / b);
+        document.getElementById("root2").innerHTML += "N/A";
         console.log(c / b);
         return;
     }
@@ -22,24 +24,26 @@ function calculate()
     if (((Math.pow(b , 2)) - 4 * a * c) < 0)
     {
         document.getElementById("root1").innerHTML
-            = "X1 = " + String(-b / (2 * a)) + " + "
-            + String(Math.sqrt(-(Math.pow(b , 2) - 4 * a * c)) / (2 * a)) + "i";
+            += String(((-b / (2 * a)).toFixed(DECIMAL_PLACES))) + " + "
+            + String((Math.sqrt(-(Math.pow(b , 2) - 4 * a * c)) / (2 * a)).toFixed(DECIMAL_PLACES)) + "i";
 
         document.getElementById("root2").innerHTML
-            = "X2 = " + String(-b / (2 * a)) + " - "
-            + String((Math.sqrt(-(Math.pow( b , 2 ) - 4 * a * c)) / (2 * a))) + "i";
+            += String((-b / (2 * a)).toFixed(DECIMAL_PLACES)) + " - "
+            + String(
+                ((Math.sqrt(-(Math.pow(b, 2) - 4 * a * c))
+                / (2 * a))
+            ).toFixed(DECIMAL_PLACES)) + "i";
         return;
     }
 
     document.getElementById("root1").innerHTML
-        = "X1 = " + String((-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a));
+        += String(((-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a)).toFixed(DECIMAL_PLACES));
     document.getElementById("root2").innerHTML
-        = "X2 = " + String((-b - Math.sqrt(Math.pow( b , 2 ) - 4 * a * c)) / (2 * a));
+        += String(((-b - Math.sqrt(Math.pow( b , 2 ) - 4 * a * c)) / (2 * a)).toFixed(DECIMAL_PLACES));
 
 }
 
-function check_and_calculate()
-{
+function check_and_calculate() {
     if (document.getElementById("box-a").value.length === 0
         && document.getElementById("box-b").value.length === 0
         && document.getElementById("box-c").value.length === 0) {

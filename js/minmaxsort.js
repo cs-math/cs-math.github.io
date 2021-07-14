43fb431e276a5ccb7ddeb1719fa1ceea843a7e1b
Brikaa <brikaaomar@gmail.com>
2021-07-14 14:21:51 +0200

    Initial min max sort

---
+++ js/minmaxsort.js
@@ -0,0 +1,34 @@
+const NUMBER_BOXES = 5;
+
+function add_boxes()
+{
+    let i = 0;
+    while (i < NUMBER_BOXES)
+    {
+        let main_div = document.createElement('div');
+
+        let label_info = document.createElement('label');
+        label_info.setAttribute('class', 'info')
+        label_info.innerHTML = `x<sub>${i + 1}</sub> `
+
+        let text_box = document.createElement('input');
+        text_box.setAttribute('type', 'number');
+        text_box.setAttribute('class', 'number-box');
+
+        let label_min_max = document.createElement('label');
+        label_min_max.setAttribute('class', 'info');
+
+        let button_div = document.getElementsByClassName('button-div')[0];
+
+        main_div.appendChild(label_info);
+        main_div.appendChild(text_box);
+        equation_box = document.getElementsByClassName('equation-box')[0];
+        equation_box.insertBefore(main_div, button_div);
+        ++i;
+    }
+}
+
+function sort()
+{
+    let boxes = document.getElementsByClassName('number-box');
+}

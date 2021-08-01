decf62106462d77529e1a6d8e03057b78c0cca84
Brikaa <brikaaomar@gmail.com>
2021-08-01 20:55:14 +0200

    Add matrix properties

--- js/global.js
+++ js/global.js
@@ -6,6 +6,12 @@ function set_labels(labels_map) {
     }
 }
 
+function add_to_labels(labels_map) {
+    for (let key of Object.keys(labels_map)) {
+        document.getElementById(key).innerHTML += labels_map[key];
+    }
+}
+
 function clear_elements(class_names) {
     for (let name of class_names) {
         let elements = document.getElementsByClassName(name);
--- js/matrix.js
+++ js/matrix.js
@@ -1,5 +1,9 @@
 let next_matrix_number = 1;
 
+function clear_matrix_elements() {
+    clear_elements(['matrix-area', 'solution-label']);
+}
+
 function set_initial_matrices() {
     draw_matrix_area();
     draw_matrix_area();
@@ -8,10 +12,11 @@ function set_initial_matrices() {
 function draw_matrix_area() {
     let main_div = document.getElementsByClassName('equation-box')[0];
     let matrix_div = document.createElement('div', { class: 'matrix' });
+    let matrix_value = next_matrix_number === 1 ? '1   2  3\n4   5  6\n0.5 2 -0.25' : '';
     matrix_div.innerHTML = `<h5>Matrix ${String(next_matrix_number)}</h5>
-        <p class="instructions">Elements are separated by a space,<br />
-        rows are separated by a new line</p>
-        <textarea class="matrix-area" id="matrix${String(next_matrix_number)}"></textarea>`;
+        <textarea class="matrix-area" id="matrix${String(
+            next_matrix_number
+        )}">${matrix_value}</textarea>`;
     main_div.appendChild(matrix_div);
     ++next_matrix_number;
 }
@@ -25,9 +30,59 @@ function matrix_area_to_2d(matrix_area) {
     return two_dimensions;
 }
 
+function validate_matrix(matrix) {
+    let first_row_length = matrix[0].length;
+    for (let row of matrix) {
+        if (row.length !== first_row_length) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function is_symmetric_matrix(matrix) {
+    for (let i = 0; i < matrix.length; ++i) {
+        for (let j = 0; j < matrix.length; ++j) {
+            if (matrix[i][j] === matrix[j][i]) {
+                continue;
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
 function get_matrices_info() {
+    /*
+    Symmetry: a11 = a11, a12 = a21, a13 = a31, etc.
+    */
+    clear_elements(['solution-label']);
     let matrices_areas = document.getElementsByClassName('matrix-area');
-    for (let matrix_area of matrices_areas) {
-        matrix_area_to_2d(matrix_area);
+    for (let i = 0; i < matrices_areas.length; ++i) {
+        let matrix = matrix_area_to_2d(matrices_areas[i]);
+        if (matrix[0].length === 0 && matrix.length === 1) {
+            continue;
+        }
+        if (!validate_matrix(matrix)) {
+            return set_labels({
+                validation: `The numbers of elements in each column do not match in matrix #${
+                    i + 1
+                }`
+            });
+        }
+        let is_square = matrix[0].length === matrix.length;
+        let is_symmetric = is_square ? is_symmetric_matrix(matrix) : false;
+        if (i !== 0) {
+            add_to_labels({
+                shape: ', ',
+                symmetry: ', ',
+                order: ', '
+            });
+        }
+        add_to_labels({
+            shape: is_square ? 'Square' : 'Rectangular',
+            symmetry: is_symmetric ? 'Symmetric' : 'Asymmetric',
+            order: matrix[0].length + ' x ' + matrix.length
+        });
     }
 }
